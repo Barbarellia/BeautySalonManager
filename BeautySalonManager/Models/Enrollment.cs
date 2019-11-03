3632185805f2bf50f52e7e2ed1a9245c87dbb753
Barbarellia <basiasz30@gmail.com>
2019-11-03 23:07:36 +0100

    Add project files.

---
+++ BeautySalonManager/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautySalonManager.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TreatmentAssignmentId { get; set; }
+        [Display(Name = "Data")]
+        public DateTime Date { get; set; }
+        [Display(Name = "Czy wykonano")]
+        public bool Active { get; set; }
+
+        public TreatmentAssignment TreatmentAssignment { get; set; }
+        //public User User { get; set; }
+    }
+}
