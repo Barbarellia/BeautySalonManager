d4cda93f8a28713e272bede590841963762da229
Barbara Szweda <basiasz30@gmail.com>
2019-11-06 15:53:30 +0100

    Added data for database, scaffolded employees and treatments, updated the main page

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using BeautySalonManager.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BeautySalonManager.Models;
 
 namespace BeautySalonManager
 {
@@ -36,6 +37,9 @@ namespace BeautySalonManager
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalonContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalonContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
