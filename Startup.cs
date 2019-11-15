386a3cd33b1cb78a89632d8d382024fa77fb114a
Barbara Szweda <basiasz30@gmail.com>
2019-11-15 15:48:42 +0100

    fix to start the page, remove migrations and context

--- Startup.cs
+++ Startup.cs
@@ -28,13 +28,12 @@ namespace BeautySalonManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for 
-                //non -essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddDbContext<SalonContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<SalonContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
